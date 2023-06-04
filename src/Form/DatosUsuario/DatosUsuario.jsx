659cb5829c6c294875e547501faeeeb0dcb1f4f2
K3yJey <kevin_madrid10@hotmail.es>
2023-06-04 14:03:31 -0500

    feat: :zap: Se trabajó con el archivo de DatosUsuario y validaciones

--- src/Form/DatosUsuario/DatosUsuario.jsx
+++ src/Form/DatosUsuario/DatosUsuario.jsx
@@ -1,40 +1,70 @@
-import React from "react"
+import React, { useState } from "react"
 import { TextField, Button, Box } from "@mui/material"
+import { validarEmail, validarPassword } from "./Validaciones"
 
-class DatosUsuario extends React.Component {
-  render() {
-    return (
-      <Box
-        component="form"
-        autocomplete="off"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column"
-        }}>
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="email"
-          error={false}
-          helperText={false && "Ingresa un correo electrónico válido"}
-        />
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-        />
-        <Button variant="contained" type="submit">
-          Siguiente
-        </Button>
-      </Box>
-    )
-  }
+const DatosUsuario = () => {
+  const [email, setEmail] = useState({ value: "", valid: true })
+  const [password, setPassword] = useState({ value: "", valid: true })
+
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column"
+      }}
+      onSubmit={(e) => {
+        e.preventDefault()
+        if (email && password == true) {
+          console.log("Avanza")
+        } else {
+          console.log("No avanza")
+        }
+      }}>
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={email.valid == false}
+        helperText={email.valid == false && "Ingresa un correo electrónico válido"}
+        value={email.value}
+        onBlur={() => {
+          validarEmail(email)
+        }}
+        onChange={(input) => {
+          const email = input.target.value
+          setEmail({ value: input.target.value, valid: validarEmail(email) })
+        }}
+      />
+
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        error={password.valid == false}
+        helperText={password.valid == false && "Ingresa una contraseña válida"}
+        value={password.value}
+        onChange={(input) => {
+          const password = input.target.value
+          setPassword({
+            value: input.target.value,
+            valid: validarPassword(password)
+          })
+        }}
+      />
+
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  )
 }
 
 export default DatosUsuario
